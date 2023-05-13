1553ef2ad333e58ca6a0e99b0b07defdc1b6fb83
Anik <naimur.rezaa@gmail.com>
2023-05-13 18:01:06 +0600

    last commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("coffeeDB").collection("coffee");
     // Send a ping to confirm a successful connection
 
